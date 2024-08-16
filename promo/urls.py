5cd1d422d0d151ab1f1dea0a090e14b084252ee2
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-08-16 03:44:06 -0700

    update

--- promo/urls.py
+++ promo/urls.py
@@ -2,13 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
-router = DefaultRouter()
-router.register(r'promo', PromoAPIView, basename='promo')
-# router.register(r'promo-entries', PromoCountViewSet,)
+
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('promo/', PromoAPIView.as_view()),
     path('promo-entries/monthly/', PromoMonthlyView.as_view(),),
     path('promo-entries/calculate/', PromoCountViewSet.as_view({'get': 'calculate_codes'}),),
     path('fetch-promo/', FetchPromoView.as_view(),),
