7d98d2af96ccac9d31e05179e1c72ca91181af8a
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-08-16 03:58:28 -0700

    update

--- conf/settings.py
+++ conf/settings.py
@@ -60,6 +60,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 
 
 }
