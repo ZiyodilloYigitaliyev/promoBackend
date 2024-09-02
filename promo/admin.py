5002aab9695087dc4cd752963d557ffc886dfab6
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-09-02 03:56:31 -0700

    update

--- promo/admin.py
+++ promo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Promo, PromoEntry
+from .models import *
+
+admin.site.register(SMSLog)
 
 class PromoEntryInline(admin.TabularInline):
     model = PromoEntry
