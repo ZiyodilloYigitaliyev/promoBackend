7311fd90bc20d6c6a5858cc200c2976bf5bdff6d
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-09-02 06:45:50 -0700

    update

--- promo/serializers.py
+++ promo/serializers.py
@@ -6,12 +6,12 @@ class PromoEntrySerializer(serializers.ModelSerializer):
         model = PromoEntry
         fields = ['msisdn', 'opi', 'short_number', 'message']
 
-class PromoSerializer(serializers.ModelSerializer):
-    promos = PromoEntrySerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Promo
-        fields = ['id', 'tel', 'sent_count', 'promos']
+# class PromoSerializer(serializers.ModelSerializer):
+#     promos = PromoEntrySerializer(many=True, read_only=True)
+#
+#     class Meta:
+#         model = Promo
+#         fields = ['id', 'tel', 'sent_count', 'promos']
 
 # class SMSLogSerializer(serializers.ModelSerializer):
 #     class Meta:
