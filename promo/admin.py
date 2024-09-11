c1937980d0ca5fc529ac330c182b058cb20703bc
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-09-11 15:47:33 +0500

    update

--- promo/admin.py
+++ promo/admin.py
@@ -9,18 +9,18 @@ class PromoEntryAdmin(admin.ModelAdmin):
     list_display = ('text', 'created_at')
 admin.site.register(PromoEntry, PromoEntryAdmin)
 
+
+@admin.register(Promo)
 class PromoAdmin(admin.ModelAdmin):
     list_display = ['promo_text']  # Admin panelda promo matnini ko'rsatish
-    actions = ['delete_selected_promos']
+    search_fields = ['promo_text']  # Qidiruv uchun promo_text maydoni
+    actions = ['delete_selected_promos']  # Tanlangan promokodlarni o'chirish amali
 
     def delete_selected_promos(self, request, queryset):
         count = queryset.count()
         queryset.delete()
         self.message_user(request, f"{count} ta promo muvaffaqiyatli o'chirildi.")
-    delete_selected_promos.short_description = 'For delete all Promo cods'
-@admin.register(Promo)
-class PromoAdmin(admin.ModelAdmin):
-    search_fields = ['promo_text']  # Bu yerdagi 'promo_text' - qidiriladigan maydon
-admin.site.register(Promo, PromoAdmin)
+
+    delete_selected_promos.short_description = 'Tanlangan barcha Promo kodlarni o‘chirish'
 
 
