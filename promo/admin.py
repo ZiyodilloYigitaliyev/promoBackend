8f947258b0d9e01a1b18a08b9bfa6323f5e3cdc0
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-09-13 17:30:00 +0500

    update

--- promo/admin.py
+++ promo/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-
+from django.db.models import Count
 @admin.register(PostbackRequest)
 class PostbackRequestAdmin(admin.ModelAdmin):
     list_display = ('msisdn', 'opi', 'short_number', 'sent_count')
@@ -14,13 +14,31 @@ admin.site.register(PromoEntry, PromoEntryAdmin)
 class PromoAdmin(admin.ModelAdmin):
     list_display = ['promo_text']  # Admin panelda promo matnini ko'rsatish
     search_fields = ['promo_text']  # Qidiruv uchun promo_text maydoni
-    actions = ['delete_selected_promos']  # Tanlangan promokodlarni o'chirish amali
+    actions = ['delete_selected_promos', 'delete_duplicates']  # Tugmalar ro'yxati
 
+    # Tanlangan promolarni o'chirish funksiyasi
     def delete_selected_promos(self, request, queryset):
         count = queryset.count()
         queryset.delete()
         self.message_user(request, f"{count} ta promo muvaffaqiyatli o'chirildi.")
 
     delete_selected_promos.short_description = 'Tanlangan barcha Promo kodlarni o‘chirish'
+
+    # Takrorlangan promokodlarni o'chirish funksiyasi
+    def delete_duplicates(self, request, queryset=None):
+        # Takrorlangan promokodlarni aniqlash
+        duplicates = Promo.objects.values('promo_text').annotate(Count('id')).filter(id__count__gt=1)
+        count = 0
+
+        for duplicate in duplicates:
+            promo_text = duplicate['promo_text']
+            # Birinchi promokoddan tashqari qolgan hamma takrorlanganlarni o'chirish
+            Promo.objects.filter(promo_text=promo_text).exclude(
+                id=Promo.objects.filter(promo_text=promo_text).first().id).delete()
+            count += (duplicate['id__count'] - 1)
+
+        self.message_user(request, f"{count} ta takrorlangan promo muvaffaqiyatli o'chirildi.")
+
+    delete_duplicates.short_description = 'Takrorlangan Promo kodlarni o‘chirish'
 
 
