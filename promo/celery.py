bc39e6dbcd5cfd2fb53676a69cac6f9b1a30e71f
Cik232 <azamjon23202@gmail.com>
2024-11-03 19:49:30 +0500

    update

--- promo/celery.py
+++ promo/celery.py
@@ -1,8 +1,20 @@
+# promo/celery.py
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
+
+# Django konfiguratsiyalarini yuklash
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'promo.settings')
 
 app = Celery('promo')
 
-# Broker va natija backend URL'ni Redis uchun o'rnating
-app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
-app.conf.result_backend = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+# Django konfiguratsiyalarini Celery ga yuklash
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Tasklarni avtomatik aniqlash
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
